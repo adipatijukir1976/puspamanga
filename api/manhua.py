75d9737695a802767ed4541d183516f519e694a5
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 12:42:04 +0700

    Update manhua.py

--- api/manhua.py
+++ api/manhua.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from api.scrape import scrape_komik
 
-manhua_bp = Blueprint("manhua", __name__)
+manhua_bp = Blueprint("manhua", __name__, url_prefix="/api")
 
 @manhua_bp.route("/manhua")
 def get_manhua():
