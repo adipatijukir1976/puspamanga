54afacbda93a1ec5c6e2f5d8b312ae031b3fed35
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 12:42:47 +0700

    Update manhwa.py

--- api/manhwa.py
+++ api/manhwa.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from api.scrape import scrape_komik
 
-manhwa_bp = Blueprint("manhwa", __name__)
+manhwa_bp = Blueprint("manhwa", __name__, url_prefix="/api")
 
 @manhwa_bp.route("/manhwa")
 def get_manhwa():
