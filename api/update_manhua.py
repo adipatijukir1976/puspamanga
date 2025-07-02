ae7bc26b669e2be0b305b00c35bf8a80b91b1661
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 14:14:16 +0700

    Update update_manhua.py

--- api/update_manhua.py
+++ api/update_manhua.py
@@ -3,7 +3,7 @@ from api.scrape import scrape_paginated_bge
 
 update_manhua_bp = Blueprint("update_manhua", __name__, url_prefix="/api/update")
 
-@manhua_bp.route("/manhua")
+@update_manhua_bp.route("/manhua")
 def update_manhua():
     data = scrape_paginated_bge("manhua", pages=5)
     return jsonify(data)
