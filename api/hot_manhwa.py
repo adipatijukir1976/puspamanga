22c3dcdcd37bb535eacc19ffb52f465498761925
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 15:21:54 +0700

    Update hot_manhwa.py

--- api/hot_manhwa.py
+++ api/hot_manhwa.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
-from api.scrape import scrape_paginated_bge
+from api.scrape import scrape_hot_bge
 
 hot_manhwa_bp = Blueprint('hot_manhwa_bp', __name__, url_prefix="/api/hot")
 
 @hot_manhwa_bp.route('/manhwa', methods=['GET'])
 def hot_manhwa():
-    data = scrape_paginated_bge("manhwa")
+    data = scrape_hot_bge("manhwa")
     return jsonify(data)
