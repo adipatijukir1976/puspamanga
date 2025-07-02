d850f388f8f1d7417b569aa642aa6021a630d31b
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 13:48:50 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from api.manga import manga_bp
 from api.manhua import manhua_bp
 from api.manhwa import manhwa_bp
+from api.update_manga import update_manga_bp
+from api.update_manhua import update_manhua_bp
+from api.update_manhwa import update_manhwa_bp
 from api.home import home_bp
 from flask_cors import CORS
 
@@ -11,6 +14,9 @@ CORS(app)
 app.register_blueprint(manga_bp)
 app.register_blueprint(manhua_bp)
 app.register_blueprint(manhwa_bp)
+app.register_blueprint(update_manga_bp)
+app.register_blueprint(update_manhua_bp)
+app.register_blueprint(update_manhwa_bp)
 app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
