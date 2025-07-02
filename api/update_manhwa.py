7fcb1e441becafcba3ea82387611a53aa76f3b1e
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 14:14:48 +0700

    Update update_manhwa.py

--- api/update_manhwa.py
+++ api/update_manhwa.py
@@ -3,7 +3,7 @@ from api.scrape import scrape_paginated_bge
 
 update_manhwa_bp = Blueprint("update_manhwa", __name__, url_prefix="/api/update")
 
-@manhwa_bp.route("/manhwa")
+@update_manhwa_bp.route("/manhwa")
 def update_manhwa():
     data = scrape_paginated_bge("manhwa", pages=5)
     return jsonify(data)
