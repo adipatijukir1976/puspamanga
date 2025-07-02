5872980299f64b78ab214f3a53e75f2a602088a6
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 15:21:26 +0700

    Update hot_manhua.py

--- api/hot_manhua.py
+++ api/hot_manhua.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
-from api.scrape import scrape_paginated_bge
+from api.scrape import scrape_hot_bge
 
 hot_manhua_bp = Blueprint('hot_manhua_bp', __name__, url_prefix="/api/hot")
 
 @hot_manhua_bp.route('/manhua', methods=['GET'])
 def hot_manhua():
-    data = scrape_paginated_bge("manhua")
+    data = scrape_hot_bge("manhua")
     return jsonify(data)
