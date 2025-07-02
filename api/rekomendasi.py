66cf5db1f34702cd6eefd77cbf50c5ae4ec2edae
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 16:07:34 +0700

    Update rekomendasi.py

--- api/rekomendasi.py
+++ api/rekomendasi.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from api.scrape import scrape_rekomendasi_bge
 
-rekomendasi_bp = Blueprint('rekomendasi_bp', __name__, url_prefix="/api/rekomendasi")
+rekomendasi_bp = Blueprint('rekomendasi_bp', __name__, url_prefix="/api")
 
-@rekomendasi_bp.route('/manga', methods=['GET'])
+@rekomendasi_bp.route('/rekomendasi', methods=['GET'])
 def hot_manga():
     data = scrape_rekomendasi_bge()
     return jsonify(data)
