c7fc775ecd87ea6e667bb0df55932dc06bf7afd0
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 12:41:35 +0700

    Update manga.py

--- api/manga.py
+++ api/manga.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from api.scrape import scrape_komik
 
-manga_bp = Blueprint("manga", __name__)
+manga_bp = Blueprint("manga", __name__, url_prefix="/api")
 
 @manga_bp.route("/manga")
 def get_manga():
