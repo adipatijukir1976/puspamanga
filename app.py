578347297c05995d4ca0979abc527cfaf0b59206
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 14:48:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from api.manhwa import manhwa_bp
 from api.update_manga import update_manga_bp
 from api.update_manhua import update_manhua_bp
 from api.update_manhwa import update_manhwa_bp
+from api.hot_manga import hot_manga_bp
+from api.hot_manhua import hot_manhua_bp
+from api.hot_manhwa import hot_manhwa_bp
 from api.home import home_bp
 from flask_cors import CORS
 
@@ -17,6 +20,9 @@ app.register_blueprint(manhwa_bp)
 app.register_blueprint(update_manga_bp)
 app.register_blueprint(update_manhua_bp)
 app.register_blueprint(update_manhwa_bp)
+app.register_blueprint(hot_manga_bp)
+app.register_blueprint(hot_manhua_bp)
+app.register_blueprint(hot_manhwa_bp)
 app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
