fcdbc0ba487c5b653452dae3ff2553a669e7115f
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-02 14:04:25 +0700

    Update update_manga.py

--- api/update_manga.py
+++ api/update_manga.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from api.scrape import scrape_paginated_bge
 
-manga_bp = Blueprint("update_manga", __name__, url_prefix="/api/update")
+update_manga_bp = Blueprint("update_manga", __name__, url_prefix="/api/update")
 
 @manga_bp.route("/manga")
 def update_manga():
